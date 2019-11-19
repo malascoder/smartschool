f6c6ba54385be92cca2157b98b11efae8854a847
Arangga <aranggagp@gmail.com>
2019-11-19 01:15:31 -0800

    Update index.php

--- application/views/index.php
+++ application/views/index.php
@@ -265,7 +265,7 @@
                     </div>
                     <div class="card-body">
                         <div class="row">
-                        <div class="col-xl-3 col-md-6 mb-4">
+                        <!-- <div class="col-xl-3 col-md-6 mb-4">
                             <div class="card border-left-primary h-100 py-2">
                                 <div class="card-body">
                                 <div class="row no-gutters align-items-center">
@@ -279,7 +279,7 @@
                           </div>
                         </div>
                       </div>
-                    </div>
+                    </div> -->
                         
                         <div class="col-xl-3 col-md-6 mb-4">
                             <div class="card border-left-success h-100 py-2">
